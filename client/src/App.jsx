8eab974d14cd0948425750ae17309708d23ce2a9
AkilaManu <akilamanujith@gmail.com>
2025-01-01 12:26:34 +0530

    dont merge

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css'
 import BookStore from './views/BookStore';
+import Test from './views/Test';
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<BookStore />} />
+        <Route path='/Test' element={<Test />} />
       </Routes>
     </BrowserRouter>
   )
